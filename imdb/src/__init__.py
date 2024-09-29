d430468f78cf4f4514889f979c972dd688f209a3
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-09-29 12:08:16 +0200

    Add files via upload

--- imdb/src/__init__.py
+++ imdb/src/__init__.py
@@ -1,5 +1,5 @@
 from Components.Language import language
-from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 import gettext
 
 PluginLanguageDomain = "IMDb"
@@ -14,9 +14,7 @@ def _(txt):
 	if gettext.dgettext(PluginLanguageDomain, txt):
 		return gettext.dgettext(PluginLanguageDomain, txt)
 	else:
-		print("[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt))
 		return gettext.gettext(txt)
 
 
-localeInit()
-language.addCallback(localeInit)
+language.addCallback(localeInit())
